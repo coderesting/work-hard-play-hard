24c3d019618f0ba6a36d282723d5ccec5c389954
Yannick Brandt <yannick-brandt@outlook.de>
2021-09-01 21:55:38 +0200

    Added terms

--- src/login/helper.ts
+++ src/login/helper.ts
@@ -6,7 +6,6 @@ export async function login() {
 	const auth = getAuth();
 	const result = await signInWithPopup(auth, provider);
 	const user = result.user;
-	localStorage.setItem("uid", user.uid);
 	const db = getDatabase();
 	await set(ref(db, `users/${user.uid}/name`), user.displayName?.split(" ")[0]);
 	return user;
